29195061eb4f515a96f695e37a57c49590fc8e3f
stylesol <stylesol@wgc.school.nz>
2026-03-11 13:32:46 +1300

    code at end of lessom

--- Driver.java
+++ Driver.java
@@ -22,45 +22,35 @@ public class Driver
         Scanner reader = new Scanner(System.in);
         String choice = "y"; // this is for while loop
         
+
+        PigGame myGame = new PigGame();
         //player 1
         while (choice.equals("y")){
         // create dice to flip
-        PigGame myGame = new PigGame();
         
         //getting dice to flip
         myGame.roll();
     
         // gives us the numbers
-        System.out.println("Dice rolled: " + myGame.getRoll()); 
+        //System.out.println("Dice rolled: " + myGame.getRoll()); 
         
         // Get the dice totals - add to play total
 
-        player1 = myGame.getRoll();
-        System.out.println("Player 1s total is: " + player1);        
+        int currentRoll = myGame.getRoll();
         
         // checking number rolled
-        if (player1 == 1) {
+        if (currentRoll == 1) {
             // no points if 1 rolled
             System.out.println("You rolled a 1! ");
             System.out.println("You lose all your points");
             System.out.println(" "); // spacing beteeen code
-        } else if (player1 > 1) {
-            // if it doesnt roll 1, adds points to total
-            System.out.println("You rolled a " + myGame.getRoll());
-            player1 = player1 + myGame.getRoll();
+        } else {
+            player1 += currentRoll; // Just add it once!
             System.out.println(" "); // spacing beteeen code
-
-            System.out.println("Round total: " + player1);
-            System.out.println(" "); // spacing beteeen code
-
+            
             //asks to play again
             System.out.print("Roll again? (y/n): ");
             choice = reader.nextLine();
-        } else{
-            //idk but its telling user something not right
-            System.out.println("uh oh theres an error");
-            System.out.println(" "); // spacing beteeen code
-
         }
 
         // player 2s turn
@@ -70,9 +60,30 @@ public class Driver
         player2 = myGame.getRoll();
         System.out.println("Player 2s total is: " + player2);
         
+
+        System.out.println("Player 2's Turn ");
+        choice = "y"; // Reset so Player 2 can start 
         
-        // ask user if they want to roll again and if no bye them
-        // if yes, restart loop        
+        while (choice.equals("y")) {
+            myGame.roll();
+            int currentRoll2 = myGame.getRoll();
+            if (currentRoll2 == 1) {
+                System.out.println("You rolled a 1! ");
+                System.out.println("You lose all your points");
+                System.out.println(" "); // spacing beteeen code                choice = "n"; 
+            } else {
+                player2 += currentRoll2;
+                System.out.println("Player 2 rolled: " + currentRoll2);
+                System.out.println("Player 2 Total: " + player2);
+                System.out.print("Roll again? (y/n): ");
+                choice = reader.nextLine();
+            }
+        }
+        
+        System.out.println("Game Over!");
+        System.out.println("Final Scores");
+        System.out.println("  ");
+        ///System.out ---- players scores and who wom
     }
-    }
+}
 }
